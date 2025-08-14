869d43e919950e40a5b081f9789822ed06a1f11d
EdGoll <EdGoll@users.noreply.github.com>
2025-08-14 16:47:24 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,9 @@ Amplify.configure(outputs);
 const client = generateClient<Schema>();
 
 export default function App() {
+    
+  const { signOut } = useAuthenticator();
+  
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   function listTodos() {
@@ -47,6 +50,7 @@ export default function App() {
           Review next steps of this tutorial.
         </a>
       </div>
+            <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
