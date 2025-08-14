702027d834d892ea45297b4ce8fb7a64cba147df
EdGoll <EdGoll@users.noreply.github.com>
2025-08-14 16:44:49 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,11 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./app.css";
 
+import AuthenticatorWrapper from "./AuthenticatorWrapper";
+import "@aws-amplify/ui-react/styles.css";
+
+import outputs from "@/amplify_outputs.json";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,8 +20,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
+    
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>      
+        <AuthenticatorWrapper>
+          {children}
+        </AuthenticatorWrapper>
+      </body>
     </html>
   );
 }
